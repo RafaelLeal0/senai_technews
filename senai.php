c1eed1ef37bff82951cfdae3bf3cb1bdc47d5ff0
rianeduzz <rianedupz@gmail.com>
2025-02-20 13:38:21 -0300

    tome

---
+++ senai.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SENAI Taubaté</title>
+    <link rel="stylesheet" href="<?php echo $css_path; ?>">
+    <link rel="stylesheet" href="senai.css">
+</head>
+<body>
+    <header>
+        </header>
+        <img src="./img/bunner_senaitaubate(png).png" alt="Banner SENAI Taubaté" class="banner">
+
+    <main>
+        <h1 class="titulo">SOBRE O SENAI TAUBATÉ</h1>
+        <p class="descricao">O SENAI é um dos cinco maiores complexos de educação profissional do mundo e o maior da América Latina. A rede SENAI-SP engloba 92 unidades fixas, incluindo a escola SENAI Félix Guisard, localizada na cidade de Taubaté.</p>
+        <p class="descricao">Oferecemos cursos para as qualificações de mecânica, metalurgia, eletricidade, eletrônica, automação, segurança do trabalho, logística, gestão, tecnologia da informação, entre outras. Contamos com laboratórios de metrologia, hidráulica, mecânica, impressão 3D, informática industrial, automação, eletrônica, elétrica, ferramentaria e plásticos.</p>
+        <p class="descricao">O SENAI-SP tem a missão de impulsionar o aumento da competitividade da indústria por meio da educação profissional, inovação, tecnologia e desenvolvimento industrial sustentável. Com esse objetivo em mente, o SENAI-SP investe constantemente em laboratórios de última geração e na formação inicial profissional de alta qualidade.</p>
+    </main>
+</body>
+</html>
