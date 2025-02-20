36ccfc0b3765f974f9b6f43ecb83d6a5adf61f74
GustaHenrique <gutiloloeju@gmail.com>
2025-02-20 12:01:46 -0300

    alterações

---
+++ ds.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso Técnico - Desenvolvimento de Sistemas</title>
+    <link rel="stylesheet" href="../css/ds.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+        <header>
+            <div class="container">
+                <img src="logo.png" alt="SENAI Logo" class="logo">
+                <nav>
+                    <ul>
+                        <li><a href="#">SENAI Taubaté</a></li>
+                        <li><a href="#">Curso</a></li>
+                        <li><a href="#">Notícias</a></li>
+                    </ul>
+                </nav>
+                <div class="header-links">
+                    <a href="#">✉ Email</a>
+                    <a href="#" class="login">🔒 Entrar</a>
+                </div>
+            </div>
+        </header>
+        
+        <section class="banner">
+            <img src="./img/bannerDs.png" alt="Banner">
+        
+
+        </section>
+
+        <main>
+            <section class="descricao">
+                <h1>CURSO TÉCNICO DE DESENVOLVIMENTO DE SISTEMAS</h1>
+                <p>Este curso tem como objetivo capacitar profissionais para desenvolver, testar e implantar sistemas computacionais...</p>
+            </section>
+            
+            <section class="areas-atuacao">
+                <h2>ÁREAS DE ATUAÇÃO</h2>
+                <div class="grid">
+                    <div class="area"><img src="front-end.png" alt="Front-end"></div>
+                    <div class="area"><img src="mobile.png" alt="Mobile"></div>
+                    <div class="area"><img src="back-end.png" alt="Back-end"></div>
+                    <div class="area"><img src="dba.png" alt="DBA"></div>
+                    <div class="area"><img src="pentester.png" alt="Pentester"></div>
+                    <div class="area"><img src="ciberseguranca.png" alt="Cibersegurança"></div>
+                    <div class="area"><img src="dev-ia.png" alt="Dev IA"></div>
+                    <div class="area"><img src="dev-ops.png" alt="Dev Ops"></div>
+                    <div class="area"><img src="qa-tester.png" alt="QA Tester"></div>
+                    <div class="area"><img src="scrum-master.png" alt="Scrum Master"></div>
+                    <div class="area"><img src="solucoes-ti.png" alt="Soluções TI"></div>
+                </div>
+            </section>
+        </main>
+</body>
+</html>
