3f0300a238cb3f9b26ba737e0fde526cd2a8f897
RafaelLeal0 <rleal3105@gmail.com>
2025-03-06 13:44:41 -0300

    prof pronto

--- prof.php
+++ prof.php
@@ -12,46 +12,110 @@
 </head>
 <body>
 <header>
-        <div class="container">
-            <img src="./img/senai_technews.png" alt="SENAI Logo" class="logo">
-            <nav>
-                <ul class="itens">
-                   <li><a href="index.php">CURSO</a></li>
-                    <li><a href="senai.php">SENAI TAUBATÉ</a></li> 
-                    <li><a href="noticias.php">NOTÍCIAS</a></li>
-                    <li><a href="projetos.php">PROJETOS</a></li> 
-                    
-                </ul>
-            </nav>
-        </div>
-    </header>
-    <main>
-        <h1>PROFESSORES</h1>
-        <div class="professores">
-            <div class="professor">
+    <div class="container">
+        <img src="./img/senai_technews.png" alt="SENAI Logo" class="logo">
+        <nav>
+            <ul class="itens">
+                <li><a href="index.php">CURSO</a></li>
+                <li><a href="senai.php">SENAI TAUBATÉ</a></li> 
+                <li><a href="noticias.php">NOTÍCIAS</a></li>
+                <li><a href="projetos.php">PROJETOS</a></li> 
+            </ul>
+        </nav>
+    </div>
+</header>
+<main>
+    <h1>PROFESSORES</h1>
+    <div class="professores">
+        <div class="professor">
+            <div class="foto">
+                <div class="prof">
                 <img src="./img/prof/wesleyfioreze.png" alt="Wesley Fioreze">
-                <div class="info">
-                    <h2>Wesley Fioreze</h2>
-                    <p>Estou mestrando na UNIFESP-ICT, pesquisando Cibersegurança e Blockchain em ambientes IIoT. Pós-graduado em Segurança da Informação e especialista em Banco de Dados e BI, é Analista e Desenvolvedor de Sistemas pelo IFSP. Atualmente, atua como Instrutor no SENAI Taubaté/SP, com foco em segurança, banco de dados e desenvolvimento. Seus interesses incluem Cibersegurança, Blockchain e Indústria 4.0, destacando-se por ética, comunicação, adaptabilidade e criatividade.<p>
+                </div>
+                <div class="link">
+                    <a href="https://www.linkedin.com/in/wesley-fioreze/" target="_blank">
+                        <img src="./img/logo/logo_linkedin.png"  alt="Linkedin">
+                    </a>          
+                    <a href="https://www.instagram.com/fioreze.wesley/" target="_blank">
+                        <img src="./img/logo/logo_insta.png" alt="Instagram">
+                    </a>
+                    <a href="https://github.com/fiorezewesley" target="_blank">
+                        <img src="./img/logo/logo_git.png" alt="GitHub">
+                    </a>
                 </div>
             </div>
-            <div class="professor">
-                <img src="./img/prof/greggori.jpeg" alt="Greggori Bossolan">
-                <div class="info">
-                    <h2>Greggori Bossolan</h2>
-                    <p>Tenho mais de seis anos de experiência em TI, especializado em JavaScript, Node.js e Hardware. Na Twilio, desenvolvi fluxos, plugins e functions para call centers. Também tenho experiência em gestão de equipes, logística e planejamento, além de ter atuado no Exército Brasileiro, incluindo os Jogos Olímpicos de 2016. Como Product Owner e Customer Success, trabalhei com comunicação digital via WhatsApp, SMS e voice, integrando soluções omnichannel. Utilizo ferramentas como Jira, Pipefy, Twilio, Trello e Adobe, unindo expertise técnica, gestão estratégica e inovação.</p>
-                </div>
-            </div>
-            <div class="professor">
-                <img src="./img/prof/luizcardoso.jpeg" alt="Luiz Cardoso">
-                <div class="info">
-                    <h2>Luiz Cardoso</h2>
-                    <p>Sou um profissional apaixonado por tecnologia, cristão, tenho experiência em desenvolvimento de software, ensino de informática e gestão de marketing e mídia social. Atualmente, atuo como Instrutor de Formação Profissional na Escola SENAI Taubaté, onde compartilho meu conhecimento em tecnologia com os alunos.
-                    Minhas principais competências incluem React Native, JavaScript, HTML, CSS, React, PHP, BD MySQL desenvolvimento de front-end, desenvolvimento de software, manutenção de sistemas e redes, gestão de conteúdo digital e ensino de informática.</p>
-                </div>
+            <div class="info">
+                <h2>Wesley Fioreze</h2>
+                <p>Estou mestrando na UNIFESP-ICT, pesquisando Cibersegurança e Blockchain em ambientes IIoT. Pós-graduado em Segurança da Informação e especialista em Banco de Dados e BI, é Analista e Desenvolvedor de Sistemas pelo IFSP. Atualmente, atua como Instrutor no SENAI Taubaté/SP, com foco em segurança, banco de dados e desenvolvimento. Seus interesses incluem Cibersegurança, Blockchain e Indústria 4.0, destacando-se por ética, comunicação, adaptabilidade e criatividade.</p>
             </div>
         </div>
-        </section>
-    </main>
+
+        <div class="professor">
+            <div class="foto">
+                <div class="prof">
+                <img src="./img/prof/greggori.jpeg" alt="Greggori Bossolan">
+                </div>
+                <div class="link">
+                    <a href="https://www.linkedin.com/in/greggori-bossolan-candido-a65993180/" target="_blank">
+                        <img src="./img/logo/logo_linkedin.png"  alt="Linkedin">
+                    </a>
+                    <a href="https://www.instagram.com/greggorib/" target="_blank">
+                        <img src="./img/logo/logo_insta.png" alt="Instagram">
+                    </a>
+                    <a href="https://github.com/Greggori" target="_blank">
+                        <img src="./img/logo/logo_git.png" alt="GitHub">
+                    </a>
+                </div>
+            </div>
+            <div class="info">
+                <h2>Greggori Bossolan</h2>
+                <p>Tenho mais de seis anos de experiência em TI, especializado em JavaScript, Node.js e Hardware. Na Twilio, desenvolvi fluxos, plugins e functions para call centers. Também tenho experiência em gestão de equipes, logística e planejamento, além de ter atuado no Exército Brasileiro, incluindo os Jogos Olímpicos de 2016. Como Product Owner e Customer Success, trabalhei com comunicação digital via WhatsApp, SMS e voice, integrando soluções omnichannel. Utilizo ferramentas como Jira, Pipefy, Twilio, Trello e Adobe, unindo expertise técnica, gestão estratégica e inovação.</p>
+            </div>
+        </div>
+
+        <div class="professor">
+            <div class="foto">
+                <div class="prof">
+                <img src="./img/prof/luizcardoso.jpeg" alt="Luiz Cardoso">
+                </div>
+                <div class="link">
+                    <a href="https://www.linkedin.com/in/luis-felipe-cardoso-30s/" target="_blank">
+                        <img src="./img/logo/logo_linkedin.png"  alt="Linkedin">
+                    </a>
+                    <a href="https://www.instagram.com/lfelipecardoso/" target="_blank">
+                        <img src="./img/logo/logo_insta.png" alt="Instagram">
+                    </a>
+                    <a href="https://github.com/Cardoso30S" target="_blank">
+                        <img src="./img/logo/logo_git.png" alt="GitHub">
+                    </a>
+                </div>
+            </div>
+            <div class="info">
+                <h2>Luiz Cardoso</h2>
+                <p>Sou um profissional apaixonado por tecnologia, cristão, tenho experiência em desenvolvimento de software, ensino de informática e gestão de marketing e mídia social. Atualmente, atuo como Instrutor de Formação Profissional na Escola SENAI Taubaté, onde compartilho meu conhecimento em tecnologia com os alunos.
+                Minhas principais competências incluem React Native, JavaScript, HTML, CSS, React, PHP, BD MySQL desenvolvimento de front-end, desenvolvimento de software, manutenção de sistemas e redes, gestão de conteúdo digital e ensino de informática.</p>
+            </div>
+        </div>
+        <div class="professor">
+            <div class="foto">
+                <div class="prof">
+                <img src="./img/prof/gleise.jpg" alt="Luiz Cardoso">
+                </div>
+                <div class="link">
+                    <a href="https://www.linkedin.com/in/gleise-fabiane-da-silva-rosa-7826b548/" target="_blank">
+                        <img src="./img/logo/logo_linkedin.png"  alt="Linkedin">
+                    </a>
+                    <a href="https://www.instagram.com/gleisefabiane/" target="_blank">
+                        <img src="./img/logo/logo_insta.png" alt="Instagram">
+                    </a>
+                </div>
+            </div>
+            <div class="info">
+                <h2>Glaise Rosa</h2>
+                <p>Sou Pedagoga, Analista de Sistemas e Administradora de Empresas, com experiência em educação, tecnologia e gestão. Atualmente, sou Professora de Educação Profissional no Governo do Estado de São Paulo. Atuei como Coordenadora Pedagógica no SESI-SP, professora no Colégio CEMI - COC e em funções administrativas e de suporte em TI na Unicred e Volkswagen do Brasil. Tenho expertise em formação de professores, aprendizagem organizacional e gestão educacional, integrando tecnologia e inovação para otimizar resultados.</p> 
+            </div>
+        </div>
+    </div>
+</main>
 </body>
 </html>
