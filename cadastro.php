ac5e2510a6a2387f4fa87b55fc9799803e55df41
GustaHenrique <gutiloloeju@gmail.com>
2025-04-29 13:41:20 -0300

    cadastro

--- cadastro.php
+++ cadastro.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - SENAI TechNews</title>
+    <title>Cadastro - SENAI TechNews</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap" rel="stylesheet">
@@ -12,24 +12,28 @@
 </head>
 <body>
     <div class="left">
-        <h1>BEM VINDO AO <br><span>SENAI TECHNEWS</span></h1>
+        <h1>CADASTRE-SE NO <br><span>SENAI TECHNEWS</span></h1>
     </div>
     <div class="right">
         <img src="./img/senai_technews.png" class="logo" alt="SENAI Logo">
         <form method="POST" action="">
-            <label>Email</label>
-            <input type="email" name="email" required placeholder="Coloque seu email">
-            <label>Senha</label>
-            <input type="password" name="senha" required placeholder="Coloque sua senha">
-            
-            <div class="forgot-password">
-                <a href="recuperar_senha.php">Esqueceu a senha?</a>
-            </div>
+            <label>Nome completo</label>
+            <input type="text" name="nome" required placeholder="Digite seu nome">
 
-            <button type="submit">Entrar</button>
+            <label>Email</label>
+            <input type="email" name="email" required placeholder="Digite seu email">
+
+            <label>Senha</label>
+            <input type="password" name="senha" required placeholder="Crie uma senha">
+            
+            <button type="submit">Cadastrar</button>
         </form>
+
+        <div class="forgot-password" style="margin-top: 10px; text-align: center;">
+            <span>Já tem uma conta? <a href="login.php">Entrar</a></span>
+        </div>
     </div>
-    
+
     <a href="index.php" class="back-arrow">&#8592;</a>
 </body>
 </html>
